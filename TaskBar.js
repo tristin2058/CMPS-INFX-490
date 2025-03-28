f4ba3ac58e07cb902202d86989369cdf76231607
TV12Lime <vaughanty02@gmail.com>
2025-03-28 17:47:01 -0500

    Add button to dashboard and log

--- TaskBar.js
+++ TaskBar.js
@@ -13,7 +13,7 @@ function createTaskbar() {
                 position: fixed;
                 top: 0;
                 left: 0;
-                z-index: 1000;
+                z-index: 50;
             }
             .nav-links {
                 display: flex;
